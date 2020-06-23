682c401652d426a3db112588664ce5e9b832bd2f
ningjw <413526408@qq.com>
2020-06-23 14:26:45 +0800

    初次提交

---
+++ Src/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+void main(void)
+{
+	BOARD_BootClockRUN();
+	BOARD_InitPins();
+	BOARD_InitPeripherals();
+
+	/* 初始化EventRecorder并开启*/
+	EventRecorderInitialize(EventRecordAll, 1U);
+	EventRecorderStart();
+	printf("hello\r\n");
+}
